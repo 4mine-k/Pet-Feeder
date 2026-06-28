60e6aad41f07bfbce28ee38f09457868479c0951
4mine-k <4minekali@gmail.com>
2026-06-28 01:40:15 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,7 +3,7 @@
 var firebaseConfig = {
   apiKey: "AIzaSyD7R9KlqbLPw6cFwVJmdeDdQiRc9JZN9gU",
   authDomain: "pet-feeder-e8541.firebaseapp.com",
-  databaseURL: "METS_ICI_TON_DATABASE_URL",
+  databaseURL: "https://pet-feeder-e8541-default-rtdb.europe-west1.firebasedatabase.app/",
   projectId: "pet-feeder-e8541",
   storageBucket: "pet-feeder-e8541.firebasestorage.app",
   messagingSenderId: "297618316482",
